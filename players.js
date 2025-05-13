9c0c5e8537a4e804d50eb98e502e4773adc72278
CopperCoder227 <526csteiner@frhsd.com>
2025-05-13 08:51:03 -0400

    5 cards with no info

---
+++ players.js
@@ -0,0 +1,33 @@
+const players = [
+    {
+        firstName: 'INSERT',
+        lastName: 'INSERT',
+        position: 'INSERT',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    }, {
+        firstName: 'INSERT',
+        lastName: 'INSERT',
+        position: 'INSERT',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    }, {
+        firstName: 'INSERT',
+        lastName: 'INSERT',
+        position: 'INSERT',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    }, {
+        firstName: 'INSERT',
+        lastName: 'INSERT',
+        position: 'INSERT',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    }, {
+        firstName: 'INSERT',
+        lastName: 'INSERT',
+        position: 'INSERT',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    },
+]
